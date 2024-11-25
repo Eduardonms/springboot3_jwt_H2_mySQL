7eaa189d57648a97d21aede21643357f06922764
unknown <edumasa@gmail.com>
2024-11-25 19:13:49 -0300

    Atualização de Código

--- boot-mysql-crud-JWT/src/main/java/com/javainuse/bootmysqlcrud/config/WebSecurityConfig.java
+++ boot-mysql-crud-JWT/src/main/java/com/javainuse/bootmysqlcrud/config/WebSecurityConfig.java
@@ -15,6 +15,25 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @Configuration
 public class WebSecurityConfig { // extends WebSecurityConfigurerAdapter {
 
+	private static final String[] WHITE_LIST_URL = { 
+			"/api/v1/auth/**", 
+			"/v2/api-docs", 
+			"/v3/api-docs",
+			"/v3/api-docs/**", 
+			"/swagger-resources", 
+			"/swagger-resources/**", 
+			"/configuration/ui",
+			"/configuration/security", 
+			"/swagger-ui/**", 
+			"/webjars/**", 
+			"/swagger-ui.html", 
+			"/api/auth/**",
+			"/api/test/**",
+			"/h2-console",
+			"/h2-console/**",
+			"/authenticate"
+			};
+	
 	@Autowired
 	private JwtRequestFilter jwtRequestFilter;
 
@@ -33,7 +52,7 @@ public class WebSecurityConfig { // extends WebSecurityConfigurerAdapter {
 		http.csrf(csrf -> csrf.disable())
 				.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
 				.authorizeHttpRequests(
-						auth -> auth.requestMatchers("/authenticate").permitAll().anyRequest().authenticated());
+						auth -> auth.requestMatchers(WHITE_LIST_URL).permitAll().anyRequest().authenticated());
 		http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
 		return http.build();
 	}
